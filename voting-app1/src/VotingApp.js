bc597f626796f672bb3f09c22f371c8c4c2b87d3
np <nandanpatilm15@gmail.com>
2025-06-11 15:12:15 +0530

    some formatting!

--- voting-app1/src/VotingApp.js
+++ voting-app1/src/VotingApp.js
@@ -16,25 +16,23 @@ const VotingApp =() => {
      };
 
      return(
-          <div className="p-4 max-w-md mx-auto text-center bg-white rounded-xl shadow-md space-y-4">
-               <h1 className="text-xl font-b text-center">Voting Application </h1>
+          <div>
+               <h1>Voting Application </h1>
                <ul>
                     {candidates.map((candidates,index) => (
                          <li
-                         key={index}
-                         className="flex justify-between intems-center border-b py-2">
+                         key={index}>
                          <span>{candidates.name}</span>
                          <span>{candidates.votes}votes</span>
                          <button onClick = {() => handleVote(index)}
                          disabled={voted}
-                         className={'ml-4 px-4 py-2 rounded bg-white ${voted ? "bg-grey-500": "bg-blue-500 hover:bg-blue-700"}'}
                          >vote
                          </button>
                          </li>
 
                     ))}
                 </ul>
-                {voted && <p className="text-green-400 text-center">You have voted</p>}
+                {voted && <p>You have voted</p>}
           </div>
      );
 };
